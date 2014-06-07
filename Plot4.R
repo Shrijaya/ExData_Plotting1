8d3a12ab3f1dc020aec853ec1aadbdbc61867242
Shrijaya <dey_shrijaya@yahoo.com>
2014-06-08 00:34:17 +0100

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,42 @@
+# Load data
+
+source("D:/Shrijaya/R/Exp_dataAnalysis/household_power_consumption.txt")
+
+
+
+
+
+# Open png device
+
+png(filename='plots/plot1.png')
+
+
+# Load data
+
+source("D:/Shrijaya/R/Exp_dataAnalysis/household_power_consumption.txt")
+
+
+
+# Open png device
+
+png(filename='plots/plot2.png')
+
+
+# Load data
+
+source("D:/Shrijaya/R/Exp_dataAnalysis/household_power_consumption.txt")
+
+
+# Open png device
+
+png(filename='plots/plot3.png')
+
+
+
+# Load data
+
+source("D:/Shrijaya/R/Exp_dataAnalysis/household_power_consumption.txt")
+
+# Open device
+
+png(filename='plots/plot4.png')
